0ae6751662a0b6d4ae01283023523c1582512456
maks mxwq <maks.sxwq@icloud.com>
2026-09-17 13:24:03 +0300

    add migration

---
+++ migrations/000003_users_index_change.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+DROP INDEX IF EXISTS users_email_app_id_key;
+DROP INDEX IF EXISTS users_username_app_id_key;
+
+CREATE UNIQUE INDEX IF NOT EXISTS users_email_app_id_active_key
+    ON users (email, app_id) WHERE deleted_at IS NULL;
+
+CREATE UNIQUE INDEX IF NOT EXISTS users_username_app_id_active_key
+    ON users (username, app_id) WHERE deleted_at IS NULL;
+
+-- +goose Down
+DROP INDEX IF EXISTS users_email_app_id_active_key;
+DROP INDEX IF EXISTS users_username_app_id_active_key;
+
+ALTER TABLE users ADD CONSTRAINT users_email_app_id_key UNIQUE (email, app_id);
+ALTER TABLE users ADD CONSTRAINT users_username_app_id_key UNIQUE (username, app_id);
