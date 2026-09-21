1788a68570f850793074899766c4349f5a4f3d71
maks mxwq <maks.sxwq@icloud.com>
2026-09-22 00:19:34 +0300

    add 72 hours token expiring

--- migrations/000003_create_is_verified_to_user.sql
+++ migrations/000003_create_is_verified_to_user.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 ALTER TABLE users
     ADD COLUMN is_verified BOOLEAN NOT NULL DEFAULT FALSE,
-    ADD COLUMN verify_token TEXT NOT NULL UNIQUE;
+    ADD COLUMN verify_token TEXT UNIQUE;
 
 -- +goose Down
 ALTER TABLE users
